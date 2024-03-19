4cdfaf5f66f0b8e09849871bb78c894f73df2991
Isabella Pereira <isabellapereira251@gmail.com>
2024-03-19 14:49:59 -0500

    Simulation working for 1 patient and receptionist

--- Project2/Project2.java
+++ Project2/Project2.java
@@ -1,96 +1,143 @@
+/*
+ * Isabella Pereira
+ * IAP200002
+ * CS 4348.001
+ * Greg Ozbirn 
+ * 
+ * Notes: 
+ * signal: release
+ * wait: acquire
+ */
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.Semaphore;
+
 public class Project2{
     //semaphores
+    private static Semaphore receptionist = new Semaphore(1);
+    private static Semaphore mutexFrontDesk = new Semaphore(1);
+    private static Semaphore msgFrontDesk = new Semaphore(0);
+
+    private static Queue<Integer> toReceptionist = new LinkedList<>(); 
     //array of patients
     //array of nurses
     //array of doctors
-    public static void Main(String[] args){
+
+    public static void main(String[] args){
+        //receive command line input for number of patients and doctors
+        int numPts = 1;
+        int numDrs = 1;
+
+        /*
+        Thread[] patients = new Thread[numPts];
+        Thread[] nurses = new Thread[numDrs];
+        Thread[] doctors = new Thread[numDrs];
+
+        for(int i = 0; i < numPts; i++){
+            patients[i] = new Thread(new Patient(i));
+            System.out.println("Patient " + i+ " created."); //remove later
+            patients[i].start();
+        }*/
         //create one patient thread
-
+        Thread pt = new Thread(new Patient(0));
+        pt.start();
         //create one receptionist thread
+        Thread rec = new Thread(new Receptionist());
+        rec.start();
 
     }
-}
-class Patient{
-    private static int ptNum;
-    private static int drNum;
-    public static void Main(String[] args){
-        enter();
-        //signal(waiting_room);
-        //wait(receptionist);
-        //wait(mutex_front_desk);
-        //enqueue(pt_num);
-        //signal(mutex_front_desk);
+    private static class Patient implements Runnable{
+        private int ptNum;
+        private int drNum;
+        public Patient(int ptNum){
+            this.ptNum = ptNum;
+        }
+        public void run(){
+            enter();
+            try {
+                receptionist.acquire();
+                
+                //give patient number to receptionist, critical section
+                mutexFrontDesk.acquire();
+                toReceptionist.add(ptNum);
+                mutexFrontDesk.release();
+                msgFrontDesk.release();
 
-        sit();
+                sit();
 
-        //wait(nurse);
-        //get nurse name 
-        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                //so on and so forth
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        private void enter(){
+            System.out.println("Patient " + ptNum + " enters waiting room, waits for receptionist");
+        }
+        private void sit(){
+            System.out.println("Patient " + ptNum + " leaves receptionist and sits in waiting room");
+        }
+        private void enterDrOffice(int doctorNum){
+            System.out.println("Patient " + ptNum + " enters doctor " + doctorNum + "'s office");
+        }
+        private void receiveAdvice(int doctorNum){
+            System.out.println("Patient " + ptNum + " receives advice from doctor " + doctorNum);
+        }
+        private void leave(){
+            System.out.println("Patient " + ptNum + " leaves");
+        }
+    }
+    private static class Receptionist implements Runnable{
+        private int ptNum;
+        public void run(){
+            try {
+                //wait for a message
+                msgFrontDesk.acquire();
+                //get patient number, critical section
+                mutexFrontDesk.acquire();
+                ptNum = toReceptionist.remove();
+                mutexFrontDesk.release();
+                register();
 
-        //give name to nurse
-        //wait(mutex_nurse_station[dr_num]);
-        //enqueue(pt_num)
-        //signal(mutex_nurse_station[dr_num]);
+                //receptionist.release();
 
-        //enter doctor's office
-        //wait(doctor[nurse_num]);
-        enterDrOffice(drNum);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+        private void register(){
+            System.out.println("Receptionist registers patient " + ptNum);
+        }
     }
-    private static void enter(){
-        System.out.println("Patient " + ptNum + " enters waiting room, waits for receptionist");
-    }
-    private static void sit(){
-        System.out.println("Patient " + ptNum + " leaves receptionist and sits in waiting room");
-    }
-    private static void enterDrOffice(int doctorNum){
-        System.out.println("Patient " + ptNum + " enters doctor " + doctorNum + "'s office");
-    }
-    private static void receiveAdvice(int doctorNum){
-        System.out.println("Patient " + ptNum + " receives advice from doctor " + doctorNum);
-    }
-    private static void leave(){
-        System.out.println("Patient " + ptNum + " leaves");
-    }
-}
-class Receptionist{
-    private static int ptNum;
-    public static void Main(String[] args){
-        //while(true){
-            //wait(waiting_room);
-            //wait(mutex_front_desk);
-            //dequeue_front_desk(pt_num);
-            //signal(mutex_front_desk);
-            //register(pt_num);
-            //signal(sit); //unnecessary, i think, but we’ll see :)
-            //signal(ready_for_nurse); //receptionist tells nurse
-            //signal(receptionist);
-        //}
-    }
-    private static void register(){
-        System.out.println("Receptionist registers patient " + ptNum);
-    }
-}
-class Nurse{
-    private static int ptNum;
-    private static int nurseNum;
-    public static void Main(String[] args){
+    class Nurse implements Runnable{
+        private int ptNum;
+        private int doctorNum;
+        public Nurse(int doctorNum){
+            this.doctorNum = doctorNum;
+        }
+        public void run(){
+    
+        }
+        //Nurse and doctor pair share the same number
+        private void takeToDoctor(){
+            System.out.println("Nurse " + doctorNum + " takes patient " + ptNum + " to doctor's office");
+        }
     
     }
-    //Nurse and doctor pair share the same number
-    private static void takeToDoctor(){
-        System.out.println("Nurse " + nurseNum + " takes patient " + ptNum + " to doctor's office");
+    class Doctor implements Runnable{
+        private int ptNum;
+        private int doctorNum;
+        public Doctor(int doctorNum){
+            this.doctorNum = doctorNum;
+        }
+        public void run(){
+    
+        }
+        private void listen(){
+            System.out.println("Doctor " + doctorNum + " listens to symptoms from patient " + ptNum);
+        }
+    
     }
-
-}
-class Doctor{
-    private static int ptNum;
-    private static int doctorNum;
-    public static void Main(String[] args){
-        
-        //wait(ready_for)
-    }
-    private static void listen(){
-        System.out.println("Doctor " + doctorNum + " listents to symptoms from patient " + ptNum);
-    }
-
 }
